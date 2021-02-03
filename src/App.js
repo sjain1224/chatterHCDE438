aba519f66f470a948f23343cffc7592012770ea8
Siddhant Jain <jainsid@uw.edu>
2021-02-03 11:34:28 -0800

    Class Updates Wed Feb 3 plus NamePicker starter code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import TextInput from './Textinput';
 import Message from './Message';
+import NamePicker from './NamePicker'
 
 function App() {
   const [messages,setMessages] = useState([])
@@ -10,6 +11,9 @@ function App() {
     <header className="header">
       <div className="logo" />
       Chatter
+
+      <NamePicker />
+
     </header>
 
     <main className="view-messages">
