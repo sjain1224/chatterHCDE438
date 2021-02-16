feca552eac5f0884ec4e449c51bfb393b0d51599
Siddhant Jain <jainsid@uw.edu>
2021-02-16 12:51:22 -0800

    Updates Wed 2 10 Class Firebase addition and rooms

--- src/App.js
+++ src/App.js
@@ -4,10 +4,23 @@ import TextInput from './Textinput'
 import Message from './Message'
 import NamePicker from './NamePicker'
 import {db, useDB} from './db'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
+function Wrap () {
+  return <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route exact path="/:room" component={App} />
+    </Switch>
+  </BrowserRouter>
+}
+
+function App(props) {
+
+  const room = props.match.params.room || 'home'
+
   //const [messages,setMessages] = useState([])
-  const messages = useDB()
+  const messages = useDB(room)
   const [username, setUsername] = useState(localStorage.getItem('username') || '')
 
   return <div className="App">
@@ -31,9 +44,9 @@ function App() {
 
     <TextInput 
       //send={(t)=> setMessages([{text:t, name:username, date:new Date()}, ...messages])}
-      send={(t)=> db.send({text:t, name:username, date:new Date()})}
+      send={(t)=> db.send({text:t, name:username, date:new Date(), room})}
     />
 
   </div>
 }
-export default App;
+export default Wrap;
